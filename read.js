0028a2a47b60f9c6cc7658f808ba9651720b29fe
Philip Song <philjunsong@gmail.com>
2021-07-20 23:38:23 -0700

    rift herald stat

--- read.js
+++ read.js
@@ -12,15 +12,26 @@ var gameObjs = [];
 
 console.log(gameObjs.length);
 
-var numberOfRiftHeralds = gameObjs.reduce((x, y) => {
+//BLUE => team id 100
+//RED  => team id 200
+
+var riftHeraldStats = gameObjs.reduce((x, y) => {
   console.log('numbers so far', x);
   console.log('game id', y.gameId);
+  console.log('teams win', y.teams[0].win === 'Win' ? 'blue team win': 'red team win');
+  let blueTeamWin = y.teams[0].win === 'Win';
+  let redTeamWin = y.teams[0].win === 'Win';
   console.log('number of rift herald by blue', y.teams[0].riftHeraldKills);
   console.log('number of rift herald by red', y.teams[1].riftHeraldKills);
   console.log('total rift:', y.teams[0].riftHeraldKills + y.teams[1].riftHeraldKills);
 
-  return x + y.teams[0].riftHeraldKills;
+  // return x + y.teams[0].riftHeraldKills;
+  x.numRed += y.teams[1].riftHeraldKills;
+  x.numBlue += y.teams[0].riftHeraldKills;
+  x.numRedWin += redTeamWin;
+  x.numBlueWin += blueTeamWin;
+  return x;
 
-},0);
+},{numRed: 0, numBlue: 0, numRedWin: 0, numBlueWin: 0});
 
-console.log('numberOfRiftHeralds', numberOfRiftHeralds);
+console.log('riftHeraldStats', riftHeraldStats);
